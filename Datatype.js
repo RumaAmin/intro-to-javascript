45232949a3faad896ef40c7fd77e9ab804954105
Ruma Akter <rumacse61@gmail.com>
2026-04-10 21:02:10 +0600

    Different types of Data

---
+++ Datatype.js
@@ -0,0 +1,12 @@
+var greeting ="Good Morning";
+
+var isRich = true;
+
+var age = 48;
+
+var price = 45.30;
+
+console.log(typeof greeting);
+console.log(typeof isRich);
+console.log(typeof age);
+console.log(typeof price);
