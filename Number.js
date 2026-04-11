3fd5a233a6403a5cef3f2238b5a022f05fb61132
Ruma Akter <rumacse61@gmail.com>
2026-04-11 18:51:55 +0600

    Example of Numbers

---
+++ Number.js
@@ -0,0 +1,23 @@
+ var applePrice = 20;
+
+ var mangoPrice = 30;
+
+ var lichiPrice ='50';
+
+ var newInt = parseInt(lichiPrice);
+
+ var bananaPrice =67.50;
+ var newNum = parseFloat("kashduafhjdahf");
+  var newNum2 = parseInt("kashduafhjdahf");
+
+   var mangoFloat = parseFloat(mangoPrice);
+
+   var fixFloat = mangoFloat.toFixed(5);
+
+ console.log(applePrice + mangoPrice);
+  console.log( typeof (applePrice + lichiPrice));
+  console.log(newInt);
+  console.log(newNum);
+  console.log(newNum2);
+   console.log(typeof mangoFloat);
+   console.log(fixFloat);
